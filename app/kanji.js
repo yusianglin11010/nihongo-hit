893b6537d042b456a2052646e1f6c334a64e2f27
Devin Lin <yusianglin11010@gmail.com>
2022-12-30 23:02:59 +0800

    init commit

---
+++ app/kanji.js
@@ -0,0 +1,41 @@
+// TODO: load data from Google sheet
+let dict = {
+    "何": "なに", 
+    "誰": "だれ",
+    "私":"わたし",
+    "貴方":"あなた",
+    "食べ":"たべ",
+    "旅行":"りょうこ",
+    "仕事":"しごと",
+}
+
+let kanjiList = []
+
+for (const [key, _] of Object.entries(dict)){
+    kanjiList.push(key);
+}
+
+function checkAnswer(e) {
+    question = document.getElementById("kanji-question")
+    questionColor = document.querySelector(".kanji-question-gray")
+    if (e.target.value == "") {
+        questionColor.style.backgroundColor = "#444648"
+    }
+    else if (e.target.value == dict[question.textContent]) {
+        questionColor.style.backgroundColor = "#C8E6C9"
+    } else {
+        questionColor.style.backgroundColor = "#FFCDD2"
+    }
+}
+
+
+function pickQuestion() {
+    question = document.getElementById("kanji-question")
+    randomElement = kanjiList[Math.floor(Math.random() * kanjiList.length)]
+    question.textContent = randomElement
+    document.getElementById("submit-answer").value = ""
+}
+
+document.getElementById("submit-answer").addEventListener("input", checkAnswer)
+
+window.addEventListener("keypress", pickQuestion)
